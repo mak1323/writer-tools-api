9a76139aefcbc7c1dae011d59a337db19b047c7d
mak1323 <myleskaeding@gmail.com>
2017-07-24 14:00:03 -0400

    Test worked
    API is up and running. Favorites shows the id and index. Everything is
    working locally. API is connected locally to front end.

--- scripts/favorites/000_insert_test_file_into_favorites_table.sql
+++ scripts/favorites/000_insert_test_file_into_favorites_table.sql
@@ -1,1 +1,5 @@
-INSERT INTO favorites(adjective, noun, user_id, created_at, updated_at, comment)
+INSERT INTO favorites(adjective, noun, user_id, comment)
+VALUES
+  ('Kicking', 'Chicken', 1, 'Good name for a bar.'),
+  ('Happy', 'Zombie', 2, 'Name for a movie or a book?'),
+  ('Forlorn', 'Mustard', 3, 'Codename for a project I am working on.');
